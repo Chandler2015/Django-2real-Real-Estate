a6e462520ff178c455c2fbe83162ca7e03fc4b14
YuhangXiong <yxiong4@ualberta.ca>
2019-08-12 17:54:17 -0600

    local setting

--- btre/settings.py
+++ btre/settings.py
@@ -143,12 +143,6 @@ MESSAGE_TAGS ={
     messages.ERROR:'danger',
 }
 
-#email config
-EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_PORT = 587
-EMAIL_HOST_USER = 'xiong@gmail.com'
-EMAIL_HOST_PASSWORD = '12345'
-EMAIL_USE_TLS =True
 
 try:
     from .local_settings import *
